d4749a75f2fbbe8a6ce7a5c5690ba153188373c8
edwardyzhang <edward.zhang098@gmail.com>
2024-07-31 13:58:03 -0400

    Change ReadMe

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,19 @@ function Header( ) {
     const d = new Date();
     let text = d.toDateString();
 
+    var category = 'happiness'
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+        headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
     return (<header>
        <h1> {text} </h1> <br/>
 
